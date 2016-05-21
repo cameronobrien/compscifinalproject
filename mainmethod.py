22fb356ad03afd1203974952aa31ee472dc628dd
piggiescanfly238@gmail.com <piggiescanfly238@gmail.com>
2016-05-21 11:29:59 -0500

    added locations files, extreme overhaul of the main python code

--- mainmethod.py
+++ mainmethod.py
@@ -3,7 +3,6 @@ import random
 def get_dialogue_text(choice):
     """
         :param choice(str): The dialogue tree and section of the ship that the player will explore.
-
         :returns: A list containing each dialogue item/user decision start.
     """
     file_extension = ".txt"
@@ -17,7 +16,6 @@ def get_dialogue_text(choice):
 def explore_branch(choice):
     """
         :param choice(str): The dialogue tree and section of the ship that the player will explore.
-
     """
     dialogue_tree = get_dialogue_text(choice)
     print(dialogue_tree)
@@ -26,22 +24,29 @@ def explore_branch(choice):
 
     # do game stuff for this specific branch
     while alive:
-        alive = make_choice(current_step, dialogue_tree[current_step])
+        choice = make_choice(current_step, choice)
+        print(get_dialogue_text(choice))
         current_step += 1
 
 
-def make_choice(current_step, dialogue_tree_item):
+def make_choice(current_step, choice):
     """
         :param current_step(int): Which dialogue item the player is currently on
         :param dialogue_tree_item(str): String containing the dialogue for the
                                         current step and the choice the user can make
-
         :returns: bool, alive or dead (true or false)
-
     """
+
+    file_extension = ".txt"
+
+    with open(choice + "_locations" + file_extension, 'r') as f:
+        locations = f.read().split("\n")
+
     # present decision to user
     # have the user make a decision
-    direction = input("Which direction do you go? [L/R]L").upper()
+    direction = None
+    while direction not in locations:
+        direction = input("Which direction do you go? [{}]?".format("/".join(locations))).strip()
     return direction
 
 
@@ -54,10 +59,10 @@ title_bar = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 title = "\nWelcome aboard the UNSC Aurora, the only surviving destroyer in the entire UNSC fleet.\n"
 initial_decision = "You are faced with 3 signs. One leads to the captains quarters, one leads to the logistics bay," \
 " and the other to the ship hangar.\n"
-correct_choices = ['L', 'C', 'S']
+correct_choices = ['logi', 'cq', 'shiphangar']
 print(title_bar + title + title_bar)
 choice = None  # non-valid default choice
 print(initial_decision)
 while choice not in correct_choices:
-    choice = input("Which one do you go to? [{}]: ".format("/".join(correct_choices))).upper()  # uppercase conversion
+    choice = input("Which one do you go to? [{}]: ".format("/".join(correct_choices))).strip()  # uppercase conversion
 explore_branch(choice)
